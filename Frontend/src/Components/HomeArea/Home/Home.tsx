124527416dc4f7391268cc240ea7f5e007d955b9
danil volobuyev <dani736@gmail.com>
2023-12-09 22:20:48 +0200

    fix mui

--- Frontend/src/Components/HomeArea/Home/Home.tsx
+++ Frontend/src/Components/HomeArea/Home/Home.tsx
@@ -1,13 +1,17 @@
 import './Home.css';
-import background from '../../../assets/background.jpg';
+import Button from '@mui/material/Button';
 
 function Home(): JSX.Element {
   return (
     <div className="Home">
-      <div className="backgroundContainer">
-        <img id="background" src={background}></img>
-      </div>
-      <div className="text">helloas</div>
+      <header>
+        <section className="smallNav">
+          <div>
+            {/* <Button>hello</Button> */}
+          </div>
+          <div className='logo'></div>
+        </section>
+      </header>
     </div>
   );
 }
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,11 +1,10 @@
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-import Layout from './Components/LayoutArea/Layout/Layout';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import Home from './Components/HomeArea/Home/Home';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <HashRouter>
+  <BrowserRouter>
     <Home />
-  </HashRouter>
+  </BrowserRouter>
 );
