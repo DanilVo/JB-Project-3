7cdded09255981edba887f6196dfae64df5fe7bc
DanilV <dani736@gmail.com>
2023-12-12 14:13:08 +0200

    test landing page

--- Frontend/src/Components/HeroCarousel/HeroCarousel.tsx
+++ Frontend/src/Components/HeroCarousel/HeroCarousel.tsx
@@ -1,17 +1,18 @@
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import './HeroCarousel.css';
-import hero1 from '../../assets/images/vacation_images/hero-1.jpg';
-import hero2 from '../../assets/images/vacation_images/hero-2.jpg';
-import hero3 from '../../assets/images/vacation_images/hero-3.jpg';
-import hero4 from '../../assets/images/vacation_images/hero-4.jpg';
-import { motion } from 'framer-motion';
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "./HeroCarousel.css";
+import hero1 from "../../assets/images/vacation_images/hero-1.jpg";
+import hero2 from "../../assets/images/vacation_images/hero-2.jpg";
+import hero3 from "../../assets/images/vacation_images/hero-3.jpg";
+import hero4 from "../../assets/images/vacation_images/hero-4.jpg";
+import { motion } from "framer-motion";
+import { Button } from "@mui/material";
 
 const heroImages = [
-  { imgUrl: hero1, alt: 'greece' },
-  { imgUrl: hero2, alt: 'venice' },
-  { imgUrl: hero3, alt: 'hotel' },
-  { imgUrl: hero4, alt: 'beach' },
+  { imgUrl: hero1, alt: "greece" },
+  { imgUrl: hero2, alt: "venice" },
+  { imgUrl: hero3, alt: "hotel" },
+  { imgUrl: hero4, alt: "beach" },
 ];
 
 function HeroCarousel(): JSX.Element {
@@ -20,12 +21,14 @@ function HeroCarousel(): JSX.Element {
       className="hero-carousel"
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
+      animate={{ x: -20 }}
+      transition={{ ease: "easeOut", duration: 1.5 }}
     >
       <Carousel
         showThumbs={false}
-        // autoPlay
+        autoPlay
         infiniteLoop
-        // interval={2000}
+        interval={4000}
         showArrows={false}
         showStatus={false}
       >
@@ -33,6 +36,13 @@ function HeroCarousel(): JSX.Element {
           <img src={image.imgUrl} alt={image.alt} key={image.alt} />
         ))}
       </Carousel>
+      <Button
+        className="startNow"
+        variant="contained"
+        size="large"
+      >
+        Start now
+      </Button>
     </motion.div>
   );
 }
--- Frontend/src/Components/HomeArea/Home/Home.tsx
+++ Frontend/src/Components/HomeArea/Home/Home.tsx
@@ -1,10 +1,9 @@
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import logo from '../../../assets/images/DV.png';
-import background from '../../../assets/images/background.jpg';
-import './Home.css';
-import HeroCarousel from '../../HeroCarousel/HeroCarousel';
-import { motion } from 'framer-motion';
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { motion } from "framer-motion";
+import logo from "../../../assets/images/DV.png";
+import HeroCarousel from "../../HeroCarousel/HeroCarousel";
+import "./Home.css";
 
 function Home(): JSX.Element {
   return (
@@ -22,24 +21,68 @@ function Home(): JSX.Element {
           </Button>
         </div>
       </header>
-      <div className="firstLine">
-        <motion.div
-          className="line1"
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-        >
-          <Typography variant="h3">Best Vacation on Best Price</Typography>
-          <Button className="startNow" variant="contained" size="large">
-            Start now
-          </Button>
-        </motion.div>
-      </div>
-      <div className="carouselSection">
-        <HeroCarousel />
-      </div>
-      <div className="carousel3"></div>
-      <div className="carousel4"></div>
-      <div className="carousel5"></div>
+      <main>
+        <div className="firstLine">
+          <motion.div
+            className="firstLineDiv"
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            animate={{ x: 100 }}
+            transition={{ ease: "easeOut", duration: 1.5 }}
+          >
+            <Typography variant="h3">Best Vacation On Best Price</Typography>
+            <Button className="startNow" variant="contained" size="large">
+              Start now
+            </Button>
+          </motion.div>
+        </div>
+        <div className="carouselSection">
+          <HeroCarousel />
+        </div>
+        <div className="supportAd">
+          <motion.div
+            className="supportAdDiv"
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            animate={{ x: 100 }}
+            transition={{ ease: "easeOut", duration: 1.5 }}
+          >
+            <Typography variant="h3">Anywhere, Anytime </Typography>
+            <Button className="startNow" variant="contained" size="large">
+              Start now
+            </Button>
+          </motion.div>
+        </div>
+        <div className="bookNow">
+          <motion.div
+            className="bookNowDiv"
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            animate={{ x: -100 }}
+            transition={{ ease: "easeOut", duration: 1.5 }}
+          >
+            <Typography variant="h3">Book Vacation With One Click </Typography>
+            <Button className="startNow" variant="contained" size="large">
+              Start now
+            </Button>
+          </motion.div>
+        </div>
+        <div className="dreamVacation">
+          <motion.div
+            className="dreamVacationDiv"
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+          >
+            <Typography variant="h3">
+              Make Your Dream Vacation Come To Life{" "}
+            </Typography>
+            <Button className="startNow" variant="contained" size="large">
+              Start now
+            </Button>
+          </motion.div>
+        </div>
+      </main>
+      <footer>asd</footer>
     </div>
   );
 }
